13cbdf1952763116b828e93a12db8ed91d4188d5
Joerg Beekmann <joerg@purepaces.com>
2020-10-19 10:27:18 -0700

    - yarn upgrade - use wrangler

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,41 @@
-
-var babelConfig = {
-    // More info at https://github.com/babel/babel/blob/master/packages/babel-preset-env/README.md
-    presets: [
-        ["@babel/preset-env", {
-            "modules": false,
-            "useBuiltIns": false,
-            "loose": true,
-            // Use babel-preset-env to generate JS compatible with latest Chrome V8
-            "targets": "last 2 Chrome versions"
-        }]]}
-
-module.exports = {
-    entry: {worker: ['./Worker.fsproj']},
-    target: 'webworker',
-    output: {
-        path: __dirname,
-        filename: '[name].bndl.js',
-    },
-    mode: "production",
-    // See https://github.com/fable-compiler/Fable/issues/1490
-    resolve: {symlinks: false},
-    module: {
-        rules: [
-            // - fable-loader: transforms F# into JS
-            {
-                test: /\.fs(x|proj)?$/,
-                use: {
-                    loader: "fable-loader",
-                    options: {babel: babelConfig}
-        }}]
+module.exports = env => {
+    return {
+        entry: {worker: ['./Worker.fsproj']},
+        target: 'webworker',
+        output: {
+            path: __dirname + '/worker',
+        },
+        mode: "production",
+        resolve: {
+            // See https://github.com/fable-compiler/Fable/issues/1490
+            symlinks: false
+        },
+        plugins: [ ],
+        module: {
+            rules: [
+                // - fable-loader: transforms F# into JS
+                {
+                    test: /\.fs(x|proj)?$/,
+                    use: {
+                        loader: "fable-loader",
+                        options: {
+                            babel: {
+                                // More info at https://github.com/babel/babel/blob/master/packages/babel-preset-env/README.md
+                                presets: [
+                                    ["@babel/preset-env", {
+                                        "modules": false,
+                                        "useBuiltIns": false,
+                                        "loose": true,
+                                        // Use babel-preset-env to generate JS compatible with latest Chrome V8
+                                        "targets": "last 2 Chrome versions"}
+                                    ]
+                                ]
+                            }
+                        }
+                    }
+                }
+            ]
+        }
     }
-};
+}
+
